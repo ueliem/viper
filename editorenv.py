fb3a90c958fc5cd22e05f6e2cf58331cab84b1cc
Will Long <will.long043@gmail.com>
2014-07-17 16:59:00 -0400

    Fixed indentation

--- editorenv.py
+++ editorenv.py
@@ -1,9 +1,9 @@
 class EditorEnv:
-  def __init__(self):
-    self.buf = ''
-    self.cursorv = 0
-    self.cursorh = 0
-  def clearbuf(self):
-    self.buf = ''
-  def loadbuf(self):
-    pass
+    def __init__(self):
+        self.buf = ''
+        self.cursorv = 0
+        self.cursorh = 0
+    def clearbuf(self):
+        self.buf = ''
+    def loadbuf(self):
+        pass
