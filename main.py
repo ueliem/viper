20e05a9e13cfd09e2ddc4f40dff141f5728be2be
Will Long <will.long043@gmail.com>
2014-07-17 12:47:35 -0400

    Moved code into main function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import curses
+import curses, traceback
 from modes import *
 from editorenv import *
 
@@ -7,43 +7,56 @@ def is_chr(possible):
   if possible < 256:
     return True
 
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-# curses.curs_set(0)
-stdscr.keypad(1)
+def main(stdscr):
+  dimensions = stdscr.getmaxyx()
 
-dimensions = stdscr.getmaxyx()
+  mybuf = ''
 
-mybuf = ''
+  modeman = ModeManager()
+  modeman.add_mode(NormalMode())
+  modeman.add_mode(InputMode())
+  modeman.set_mode("inpmode")
 
-modeman = ModeManager()
-modeman.add_mode(NormalMode())
-modeman.add_mode(InputMode())
-modeman.set_mode("inpmode")
+  while True:
+    if modeman.current_mode.mode_id == "inpmode":
+      # stdscr.addstr(5, 0, "Edit mode")
+      event = stdscr.getch()
+      stdscr.clear()
+      if event == 27:
+        modeman.set_mode("nrmlmode")
+      if is_chr(event):
+        mybuf += chr(event)
+      stdscr.addstr(mybuf)
+    elif modeman.current_mode.mode_id == "nrmlmode":
+      event = stdscr.getch()
+      if event == ord("q"): break
+      elif event == ord("i"):
+        modeman.set_mode("inpmode")
+    stdscr.refresh()
 
-stdscr.refresh()
-while True:
-  if modeman.current_mode.mode_id == "inpmode":
-    # stdscr.addstr(5, 0, "Edit mode")
-    event = stdscr.getch()
-    stdscr.clear()
-    if event == 27:
-      modeman.set_mode("nrmlmode")
-    if is_chr(event):
-      mybuf += chr(event)
-    stdscr.addstr(mybuf)
-  elif modeman.current_mode.mode_id == "nrmlmode":
-    event = stdscr.getch()
-    if event == ord("q"): break
-    elif event == ord("i"):
-      modeman.set_mode("inpmode")
-    # elif event == curses.KEY_UP:
-    #   stdscr.clear()
-    #   stdscr.addstr("The User Pressed UP")
-    # elif event == curses.KEY_DOWN:
-    #   stdscr.clear()
-    #   stdscr.addstr("The User Pressed DOWN")
-  stdscr.refresh()
+if __name__=='__main__':
+  try:
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    # curses.curs_set(0)
+    stdscr.keypad(1)
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    # curses.curs_set(0)
+    stdscr.keypad(1)
 
-curses.endwin()
+    stdscr.refresh()
+    main(stdscr)
+
+    stdscr.keypad(0)
+    curses.echo()
+    curses.nocbreak()
+    curses.endwin()
+  except:
+    stdscr.keypad(0)
+    curses.echo()
+    curses.nocbreak()
+    curses.endwin()
+    traceback.print_exc()
