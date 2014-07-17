332c40983a72faed20692535dde075d1f909f86c
Will Long <will.long043@gmail.com>
2014-07-17 16:59:40 -0400

    Cleaned up code.

--- main.py
+++ main.py
@@ -4,68 +4,63 @@ from modes import *
 from editorenv import *
 
 def is_chr(possible):
-  if possible < 256:
-    return True
+    if possible < 256:
+        return True
+    return False
+
+def cleanupcurses(stdscr):
+    stdscr.keypad(0)
+    curses.echo()
+    curses.nocbreak()
+    curses.endwin()
 
 def main(stdscr):
-  dimensions = stdscr.getmaxyx()
-
-  mybuf = ''
-
-  modeman = ModeManager()
-  modeman.add_mode(NormalMode())
-  modeman.add_mode(InputMode())
-  modeman.set_mode("inpmode")
-  topmostlinenum = 0
-  while True:
-    if modeman.current_mode.mode_id == "inpmode":
-      # stdscr.addstr(5, 0, "Edit mode")
-      event = stdscr.getch()
-      if event == 27:
-        modeman.set_mode("nrmlmode")
-      if event == curses.KEY_UP:
-        topmostlinenum -= 1
-      elif event == curses.KEY_DOWN:
-        topmostlinenum += 1
-
-      if is_chr(event):
-        mybuf += chr(event)
-      # stdscr.addstr(0,0, mybuf)
-      bottommostlinenum = topmostlinenum + curses.LINES
-      stdscr.erase()
-      for (index,line) in enumerate(mybuf.split("\n")[topmostlinenum:bottommostlinenum]):
-        stdscr.addstr(index, 0, line)
-
-    elif modeman.current_mode.mode_id == "nrmlmode":
-      event = stdscr.getch()
-      if event == ord("q"): break
-      elif event == ord("i"):
-        modeman.set_mode("inpmode")
-    stdscr.refresh()
+    dimensions = stdscr.getmaxyx()
+    mybuf = ''
+    modeman = ModeManager()
+    modeman.add_mode(NormalMode())
+    modeman.add_mode(InputMode())
+    modeman.set_mode("inpmode")
+    topmostlinenum = 0
+    while True:
+        if modeman.current_mode.mode_id == "inpmode":
+            # stdscr.addstr(5, 0, "Edit mode")
+            event = stdscr.getch()
+            if event == 27:
+                modeman.set_mode("nrmlmode")
+            if event == curses.KEY_UP:
+                topmostlinenum -= 1
+            elif event == curses.KEY_DOWN:
+                topmostlinenum += 1
+            if is_chr(event):
+                mybuf += chr(event)
+                # stdscr.addstr(0,0, mybuf)
+            bottommostlinenum = topmostlinenum + curses.LINES
+            stdscr.erase()
+            for (index,line) in enumerate(mybuf.split("\n")[topmostlinenum:bottommostlinenum]):
+                stdscr.addstr(index, 0, line)
+        elif modeman.current_mode.mode_id == "nrmlmode":
+            event = stdscr.getch()
+            if event == ord("q"): break
+            elif event == ord("i"):
+                modeman.set_mode("inpmode")
+        stdscr.refresh()
 
 if __name__=='__main__':
-  try:
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    # curses.curs_set(0)
-    stdscr.keypad(1)
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    # curses.curs_set(0)
-    stdscr.keypad(1)
-
-    stdscr.refresh()
-    main(stdscr)
-
-    stdscr.keypad(0)
-    curses.echo()
-    curses.nocbreak()
-    curses.endwin()
-  except:
-    stdscr.keypad(0)
-    curses.echo()
-    curses.nocbreak()
-    curses.endwin()
-    traceback.print_exc()
+    try:
+        stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        # curses.curs_set(0)
+        stdscr.keypad(1)
+        stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        # curses.curs_set(0)
+        stdscr.keypad(1)
+        stdscr.refresh()
+        main(stdscr)
+        cleanupcurses(stdscr)
+    except:
+        cleanupcurses(stdscr)
+        traceback.print_exc()
--- modes.py
+++ modes.py
@@ -1,22 +1,26 @@
 class Mode:
-  def __init__(self):
-    self.mode_id = None
+    def __init__(self):
+        self.mode_id = None
+    def on_frame(self):
+        pass
 
 class InputMode(Mode):
-  def __init__(self):
-    self.mode_id = "inpmode"
-    self.cursorv = 0
-    self.cursorh = 0
+    def __init__(self):
+        self.mode_id = "inpmode"
+        self.cursorv = 0
+        self.cursorh = 0
 
 class NormalMode(Mode):
-  def __init__(self):
-    self.mode_id = "nrmlmode"
+    def __init__(self):
+        self.mode_id = "nrmlmode"
+    def on_frame(self):
+        pass
 
 class ModeManager:
-  def __init__(self):
-    self.modes = {}
-    self.current_mode = None
-  def add_mode(self, mode):
-    self.modes[mode.mode_id] = mode
-  def set_mode(self, mode_id):
-    self.current_mode = self.modes[mode_id]
+    def __init__(self):
+        self.modes = {}
+        self.current_mode = None
+    def add_mode(self, mode):
+        self.modes[mode.mode_id] = mode
+    def set_mode(self, mode_id):
+        self.current_mode = self.modes[mode_id]
